af6ae01764d6f82e71bfefc4b4425c513b6197ca
Abhiram <58914306+Abhiram-DLVSS@users.noreply.github.com>
2021-03-25 13:09:17 +0530

    Update timetable.php

--- timetable.php
+++ timetable.php
@@ -1,8 +1,17 @@
 {
-  "Monday" : {
-    "header1" : "CS 203",
-    "header2" : "CS 205",
-    "header3" : "FL 204",
-    "header4" : "CS 203 || CS 204"
-  }
+  "Monday": [
+     {
+       "header1" : "CS 203"
+     },
+     {
+       "header2" : "CS 205"
+     },
+     {
+       "header3" : "FL 204"
+     },
+     {
+       "header4" : "CS 203 || CS 204"
+     }
+   ]
 }
+ 
