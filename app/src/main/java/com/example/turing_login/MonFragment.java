690fdd4ba992df41df83c32eaeb46ee13a4488c4
MohanKumar2301 <75250761+MohanKumar2301@users.noreply.github.com>
2021-04-08 23:11:17 +0530

    Added Faculty-1

--- app/src/main/java/com/example/turing_login/MonFragment.java
+++ app/src/main/java/com/example/turing_login/MonFragment.java
@@ -81,7 +81,7 @@ public class MonFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState){
 
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_mon, container, false);
