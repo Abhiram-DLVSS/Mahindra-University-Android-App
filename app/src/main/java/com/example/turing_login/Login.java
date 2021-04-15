5a9b3b79634930bbb776a10bc2b10dd006880511
Abhiram-DLVSS <abhiram19521@mechyd.ac.in>
2021-04-15 13:54:07 +0530

    Scroll view jumps to bottom automatically in Login & Register Activity

--- app/src/main/java/com/example/turing_login/Login.java
+++ app/src/main/java/com/example/turing_login/Login.java
@@ -2,6 +2,7 @@ package com.example.turing_login;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -11,9 +12,15 @@ import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ScrollView;
@@ -35,6 +42,7 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_Turing_Login_NoActionBar);
+
         setContentView(R.layout.login);
         email=findViewById(R.id.ID);
         password=findViewById(R.id.Password);
@@ -42,23 +50,9 @@ public class Login extends AppCompatActivity {
         Login=findViewById(R.id.Login);
         signup=findViewById(R.id.signup);
         ScrollView scrollView=findViewById(R.id.loginscroll);
+        ConstraintLayout constraintLayout;
 
-//        email.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                scrollView.fullScroll(scrollView.FOCUS_DOWN);
-//                scrollView.smoothScrollTo(signup.getLeft(), signup.getTop());
-//                Toast.makeText(Login.this, "1", Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
-//        email.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-//            @Override
-//            public void onFocusChange(View v, boolean hasFocus) {
-//                scrollView.smoothScrollTo(signup.getLeft(), signup.getTop());
-//                Toast.makeText(Login.this, "2", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+
         ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
         if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
                 connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
@@ -68,10 +62,28 @@ public class Login extends AppCompatActivity {
         else
             connected = false;
 
+        constraintLayout=findViewById(R.id.loginview);
+        constraintLayout.getViewTreeObserver().addOnGlobalLayoutListener(new  ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                scrollView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        View view = scrollView.getChildAt(scrollView.getChildCount() - 1);
+                        int bottomDetector = view.getBottom() - (scrollView.getHeight() + scrollView.getScrollY());
+                        if(bottomDetector == 0 )
+                            Log.d("loginact", "bd="+bottomDetector);
+                        else
+                        scrollView.fullScroll(View.FOCUS_DOWN);
+                    }
+                });
+            }
+        });
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("Select", "Selected Signup");
                 openRegister();
             }
         });
@@ -93,21 +105,21 @@ public class Login extends AppCompatActivity {
                 Drawable errorIcon = getResources().getDrawable(R.drawable.null_layout);
                 errorIcon.setBounds(new Rect(0, 0, errorIcon.getIntrinsicWidth(), errorIcon.getIntrinsicHeight()));
 
-                if(TextUtils.isEmpty(remail)){
-                    email.setError("Please enter your Email-ID");
-                    Toast.makeText(com.example.turing_login.Login.this, "Please enter your Email-ID", Toast.LENGTH_SHORT).show();
-                }
-                else if(TextUtils.isEmpty(rpassword)){
-                    password.setError("Please enter Password",errorIcon);
-                    Toast.makeText(com.example.turing_login.Login.this, "Please enter your Password", Toast.LENGTH_SHORT).show();
-                }
-
-                else if(!connected){
+//                if(TextUtils.isEmpty(remail)){
+//                    email.setError("Please enter your Email-ID");
+//                    Toast.makeText(com.example.turing_login.Login.this, "Please enter your Email-ID", Toast.LENGTH_SHORT).show();
+//                }
+//                else if(TextUtils.isEmpty(rpassword)){
+//                    password.setError("Please enter Password",errorIcon);
+//                    Toast.makeText(com.example.turing_login.Login.this, "Please enter your Password", Toast.LENGTH_SHORT).show();
+//                }
+                if(!connected){
                     Toast.makeText(Login.this, "Check your Internet Connection", Toast.LENGTH_SHORT).show();
                 }
-
+                else if(TextUtils.isEmpty(remail)||TextUtils.isEmpty(rpassword)){
+                    Toast.makeText(com.example.turing_login.Login.this, "Incorrect Username or Password.", Toast.LENGTH_LONG).show();
+                }
                 else{
-
                     nDialog = new ProgressDialog(com.example.turing_login.Login.this);
                     nDialog.setMessage("Glad to see you again!");
                     nDialog.setIndeterminate(false);
@@ -117,25 +129,18 @@ public class Login extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             //Login and save session
 
-                            if(task.isSuccessful())
-                            {
+                            if(task.isSuccessful()){
                                 startActivity(new Intent(getApplicationContext(), Features.class));
                                 nDialog.dismiss();
                                 finish();
                             }
                             else {
                                 nDialog.dismiss();
-                                Toast.makeText(com.example.turing_login.Login.this, "Please check your Password", Toast.LENGTH_LONG).show();
+                                Toast.makeText(com.example.turing_login.Login.this, "Incorrect Username or Password.", Toast.LENGTH_LONG).show();
                             }
-
                         }
                     });
-
                 }
-
-
-
-
             }
         });
     }
